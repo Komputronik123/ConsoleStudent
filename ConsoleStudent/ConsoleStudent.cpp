b54ac53cf5576faa895af4460f9a68230ebc32bd
Komputronik123 <90615420+Komputronik123@users.noreply.github.com>
2023-04-26 12:39:15 +0200

    Update ConsoleStudent.cpp

--- ConsoleStudent/ConsoleStudent.cpp
+++ ConsoleStudent/ConsoleStudent.cpp
@@ -4,8 +4,8 @@
 #include <vector>
 #include "student.hpp"
 
-
 using namespace std;
+
 
 
 
@@ -14,71 +14,78 @@ int main() {
     string sSurname;
     int sAge;
     int e;
+    int id=0;
 
     Student student;
 
     do
     {
+      
+
         int choice;
-        cout << " 1 add  " << endl;
+        cout << "1 add  " << endl;
         cout << "2 edit " << endl;
         cout << "3 delate" << endl;
-        cout << "4 preview" << endl;
+        cout << "4 view" << endl;
+        cout << "5 search" << endl;
         cin >> choice;
-
-
 
         if (choice == 1) // add +
         {
+            
+            string sName;
+            string sSurName;
+            int sAge;
+
             cout << "give me a name" << endl;
             cin >> sName;
             cout << "give me a suranme" << endl;
             cin >> sSurname;
             cout << "give me a age" << endl;
             cin >> sAge;
-    
 
-           
-          
-               
-                ofstream foutput;
-                ifstream finput;
-                finput.open("student.txt");
-                foutput.open("student.txt", ios::app);
+            ofstream foutput;
+            ifstream finput;
+            finput.open("student.txt");
+            foutput.open("student.txt", ios::app);
 
-                if (finput.is_open())
-                    foutput << sName << " " << sSurname << " " << sAge << endl;
-
+            if (finput.is_open()) {
+                id++;
+                foutput << "ID:" << id << " NAME:" << sName << " SURNAME:" << sSurname << " AGE:" << sAge << endl;
+            }
                 
 
-                finput.close();
-                foutput.close();
-            
-            
+            finput.close();
+            foutput.close();
+            if (finput.is_open()==false and foutput.is_open()==false) { id++; }
 
 
         }
-        else if (choice == 2) {  //edit
-                string edit;
-                string line;
-                vector<string> lines;
+        
+        
+       
 
-                ifstream fin("student.txt");
-                ofstream temp("temp.txt");
+ else if (choice == 2) {  //edit
+            string edit;
+            string line;
+            vector<string> lines;
 
-                while (getline(fin, line)) {
-                    lines.push_back(line);
-                }
-                fin.close();
+            ifstream fin("student.txt");
+            ofstream temp("temp.txt");
 
-                for (int i = 0; i < lines.size(); i++) {
-                    cout << i << ": " << lines[i] << endl;
-                }
+            while (getline(fin, line)) {
+                lines.push_back(line);
+            }
+            fin.close();
 
-                cout << "Which line do you want to edit: ";
-                cin >> edit;
+            for (int i = 0; i < lines.size(); i++) {
+                cout << i << ": " << lines[i] << endl;
+            }
 
-                int index_to_edit = stoi(edit);
+            cout << "Which line do you want to edit: ";
+            cin >> edit;
+
+            int index_to_edit = stoi(edit);
             if (index_to_edit >= 0 && index_to_edit < lines.size()) {
                 cout << "Enter new data:" << endl;
                 cout << "Name: ";
@@ -90,19 +97,16 @@ int main() {
 
                 lines[index_to_edit] = sName + " " + sSurname + " " + to_string(sAge);
             }
+            for (const auto& line : lines) {
+                temp << line << endl;
+            }
+            temp.close();
 
-                for (const auto& line : lines) {
-                    temp << line << endl;
-                }
-                temp.close();
-
-                remove("student.txt");
-                rename("temp.txt", "student.txt");
+            remove("student.txt");
+            rename("temp.txt", "student.txt");
         }
-        
 
 
-        
         else if (choice == 3) { // delete
             string deleteline;
             string line;
@@ -113,40 +117,39 @@ int main() {
             ofstream temp;
             temp.open("temp.txt");
 
-            
+
             while (getline(fin, line)) {
                 lines.push_back(line);
             }
             fin.close();
 
-          
+
             for (int i = 0; i < lines.size(); i++) {
                 cout << i << ": " << lines[i] << endl;
             }
 
-          
+
             cout << "Which line do you want to delete ";
             cin >> deleteline;
 
-           
+
             int index_to_delete = stoi(deleteline);
             if (index_to_delete >= 0 && index_to_delete < lines.size()) {
                 lines.erase(lines.begin() + index_to_delete);
             }
 
-         
+
             for (const auto& line : lines) {
                 temp << line << endl;
             }
             temp.close();
 
-          
+
             remove("student.txt");
             rename("temp.txt", "student.txt");
         }
-
-        else if (choice == 4) 
-        {    
+        else if (choice == 4)
+        {
             string deleteline;
             string line;
             vector<string> lines;
@@ -162,18 +165,43 @@ int main() {
             }
             fin.close();
 
-
             for (int i = 0; i < lines.size(); i++) {
                 cout << i << ": " << lines[i] << endl;
             }
+        }
+        else if (choice == 5)
+        {
 
 
+            
+            string line;
+            int offset;
+            string x = "";
+            ifstream Myfile("student.txt");
+            cout << " ID to search  ";
+            cin >> x;
+            /*
+            while (getline(Myfile, line)) {
+                if ((offset = line.find(x, 0)) != string::npos) {
+                    cout << line << endl;
+                }
+            }
+            */
+            while (getline(Myfile, line)) {
+                size_t found = line.find("ID:"+x);
+                if (found != string::npos) { cout << line << endl; }
+                else { cout << "This id does not exist" << endl; break; }
+               //else { cout << "bbom!"; }
+
+            }
+            Myfile.close();
+            
         }
         cout << " 1) repeat" << endl;
         cin >> e;
     } while (e == 1);
 
-        return 0;
+    return 0;
 }
     
 
